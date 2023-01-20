77ca513b4c8101a5d7922e76b7f0a8a8deab78bf
Ojochogwu <07afrik@gmail.com>
2023-01-20 01:30:20 +0100

    --nw

--- src/views/Pages/History.vue
+++ src/views/Pages/History.vue
@@ -1,14 +1,14 @@
 <template>
   <div
-    class="w-10/12 mt-8 m-auto flex justify-start items-start flex-col text-left"
+    class="w-4/5 p-3 rounded-md gap-3 border border-gray-400 mt-8 m-auto flex justify-start items-start flex-col text-left"
   >
     <div class="text-sm font-semibold">Read History</div>
-    <div class="grid grid-cols-5 gap-8">
+    <div class="grid grid-cols-5 gap-10 text-sm">
       <div class="">Book Title</div>
+      <div class="">Book Author</div>
+      <div class="">Book Genre</div>
       <div class="">No of Pages</div>
       <div class="">Completion Time</div>
-      <div class="">Author</div>
-      <div class="">Book Genre</div>
     </div>
   </div>
 </template>
--- src/views/Pages/Reader.vue
+++ src/views/Pages/Reader.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="w-full flex flex-col justify-start items-start">
-    <div></div>
-    <div class="text-sm font-semibold">Hi, Olajide, Welcome Back</div>
+    <div class="p-5">
+      <div class="text-sm font-semibold">Hi, Olajide, Welcome Back</div>
+    </div>
     <div class="flex flex-col justify-center w-full items-center">
       <div class="mt-6 w-3/5 m-auto pb-5">
         <div class="text-sm font-medium">
@@ -13,10 +14,28 @@
             class="w-4/5 m-auto outline-none text-sm bg-white rounded-md border border-gray-400 p-2 mt-2"
             placeholder="e.g: Honey & Spice, Bolu Babalola"
           />
-          <button type="submit">Submit</button>
+          <button
+            class="rounded-sm ml-4 px-4 py-2 font-medium bg-boxColor text-white text-sm outline-none hover:shadow-typeBox"
+            type="submit"
+          >
+            Submit
+          </button>
         </form>
+        <div class="">
+          <ul
+            v-for="result in searchBookResults"
+            :key="result.id"
+            class="list-none shadow-reader w-full m-auto text-left z-50"
+          >
+            <li
+              class="bg-white cursor-pointer text-black text-sm font-normal border-b px-2 py-2"
+            >
+              {{ result.volumeInfo.title }}
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="flex gap-6 justify-center w-full">
+      <div class="flex gap-6 mt-5 justify-center w-full">
         <div class="p-3 rounded-md gap-3 border text-left border-gray-400 w-90">
           <div class="border-b-2 pb-1 border-boxColor text-sm">
             Set Reading Plan
@@ -46,19 +65,21 @@
       </div>
     </div>
     <History />
-    <div
+    <!-- <div
       class="w-full absolute h-full bg-black bg-opacity-75 overflow-y-scroll"
     >
-      <ul class="list-none w-1/2 m-auto bg-opacity-100 text-left">
+      <ul
+        v-for="result in searchBookResults"
+        :key="result.id"
+        class="list-none w-1/2 m-auto bg-opacity-100 text-left"
+      >
         <li
           class="bg-black bg-opacity-90 text-white text-sm font-normal border-b px-2 py-2"
-          v-for="result in searchBookResults"
-          :key="result.id"
         >
           {{ result.volumeInfo.title }}
         </li>
       </ul>
-    </div>
+    </div> -->
   </div>
 </template>
 <script>
