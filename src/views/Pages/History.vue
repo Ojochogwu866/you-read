5e1f67aff01fb65196c0cc0a9ac35f52a80165c8
Ojochogwu <07afrik@gmail.com>
2023-03-19 22:32:08 +0100

    set-prd

--- src/views/Pages/History.vue
+++ src/views/Pages/History.vue
@@ -127,8 +127,8 @@
           <div class="px-2">{{ books.pagesPerWeek }}</div>
 
           <div class="px-1">
-            <span class="cursor-pointer" @click="displayModal = true"
-              >Edit</span
+            <span class="cursor-pointer" @click="modal1 = true"
+              ><button @click="editBooks(books._id)">Edit</button></span
             >
             /
             <span class="cursor-pointer" @click="removeCurrentBook(books._id)"
@@ -137,6 +137,42 @@
           </div>
         </div>
       </div>
+      <Card v-bind:modal-1="modal1" @update:modal-1="modal1 = $event">
+        <form
+          @submit.prevent="createBook"
+          class="flex flex-col w-auto gap-y-3 bg-white shadow-typeBox px-4 py-6 rounded-md"
+        >
+          <div class="">
+            Enter Book Details Below to keep track of your Adventure
+          </div>
+          <input
+            class="w-full bg-transparent border border-gray-400 text-black outline-none p-2 rounded-sm"
+            type="text"
+            placeholder="Book title e.g purple hibiscus"
+          />
+          <input
+            class="w-full bg-transparent border border-gray-400 text-black outline-none p-2 rounded-sm"
+            type="text"
+            placeholder="Author e.g chimamanda adichie"
+          />
+          <input
+            class="w-full bg-transparent border border-gray-400 text-black outline-none p-2 rounded-sm"
+            type="text"
+            placeholder="Book Genre"
+          />
+          <input
+            type="text"
+            placeholder="total pages"
+            class="w-full bg-transparent border border-gray-400 text-black outline-none p-2 rounded-sm"
+          />
+          <button
+            class="rounded-sm px-4 py-2 font-medium bg-boxColor text-white text-sm outline-none hover:shadow-typeBox"
+            type="submit"
+          >
+            Add Book
+          </button>
+        </form>
+      </Card>
     </div>
   </div>
 </template>
@@ -151,8 +187,10 @@ export default {
   data() {
     return {
       isDrawerOpen: false,
+      modal1: false,
       displayModal: false,
       moment,
+      editBooks: {},
     };
   },
   computed: {
@@ -161,9 +199,7 @@ export default {
       return this.getUserBooks.books;
     },
   },
-  async mounted() {
-    await this.getBook();
-  },
+  async mounted() {},
   methods: {
     async removeCurrentBook(x) {
       let res = await this.$store.dispatch("remove", {
@@ -172,9 +208,9 @@ export default {
       if (res.status == 201) {
       }
     },
-    async getBook() {
+    async editBooks() {
       let res = await this.$store.dispatch("get", {
-        endpoint: `/books/`,
+        endpoint: `/book-goals/${this.getUserBooks.bookId}`,
         auth: true,
       });
       if (!!res) {
