a9a064563c88934da7a625e7acf6b78e74fbca31
Ojochogwu <07afrik@gmail.com>
2023-03-25 01:08:32 +0100

    fre

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@
     <div class="">
       <Landing />
     </div>
+    <Prelim />
     <Footer />
   </div>
 </template>
@@ -12,11 +13,13 @@
 import Nav from "@/components/Layouts/Nav.vue";
 import Landing from "./Pages/Landing.vue";
 import Footer from "@/components/Layouts/footer.vue";
+import Prelim from "./Pages/Premlim.vue";
 export default {
   name: "HomeView",
   components: {
     Nav,
     Landing,
+    Prelim,
     Footer,
   },
 };
---
+++ src/views/Pages/Premlim.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-full h-[50vh] flex justify-center items-center">
+    <div class="w-11/12 m-auto flex justify-center items-center">
+      <div class="w-1/2">
+        <p class="text-left w-10/12 text-[17px] leading-7 py-4">
+          Built to keep Readers company by keeping track of your reading
+          adventure via – Book Documentation, Book Goals, Reading Target.<br />
+          Makes reading more exciting!
+        </p>
+        <div
+          class="w-[30%] h-0 border-[2px] -top-[2px] right-0 border-b-[none] border-l-[none] border-solid border-black border-box"
+        />
+      </div>
+
+      <div class="w-1/2 relative inline">
+        <span
+          class="absolute w-[30%] h-0 border-[2px] -top-[2px] right--[50%] border-b-[none] border-l-[none] border-solid border-black border-box"
+        />
+        <span
+          class="absolute w-[30%] -bottom-[2px] left-[50%] border-t-[none] boder-r-[none] h-0 border-[2px] border-solid border-black border-box"
+        />
+        <p class="text-2xl p-5">Code is free and Open Source</p>
+      </div>
+    </div>
+  </div>
+</template>
