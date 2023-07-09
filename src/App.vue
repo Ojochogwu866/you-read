347c52983c5c293b1bd63655a9999e8f11598015
Ojochogwu Dickson <07afrik@gmail.com>
2023-07-08 18:52:12 -0700

    refactor-11

--- src/App.vue
+++ src/App.vue
@@ -14,3 +14,23 @@
   background: radial-gradient(circle, rgb(0, 5, 44) 6%, rgb(0, 2, 18) 100%, rgba(0,212,255,1) 100%);
 }
 </style>
+
+ @babel/eslint-parser@7.19.1
+    @babel/preset-env@7.12.17
+    @element-plus/icons-vue@2.1.0
+    axios@1.3.4
+    dotenv@16.0.3
+    element-plus@2.3.2
+    firebase@9.16.0
+    gsap@3.11.5
+    gsap-trial@3.11.5
+    moment@2.29.4
+    vue@3.2.13
+    vue-click-outside@1.1.0
+    vue-custom-scrollbar@1.4.4
+    vue-loader@17.0.1
+    vue-router@4.0.3
+    vue-template-compiler@2.7.14
+    vue-toast-notification@3.1.1
+    vuex@4.0.0
+    vuex-persistedstate@4.1.0
